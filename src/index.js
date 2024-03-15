361e51e7f4ba1b20f7b52a5d1c097cb60cdda274
AKSHATH P <akshathpkk@gmail.com>
2024-03-15 23:25:53 +0530

    added middlewares for the server and made custom utils to handle db connection and errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
-})
-connectDB();
+  path: "./env",
+});
+connectDB()
+  .then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERROR", error);
+        throw error
+    })
+    app.listen(8000,()=>{
+        console.log("listening to 8000 port ");
+    } )
+  })
+  .catch((error) => {
+    console.log("CONNECTION FAILED", error);
+  });
