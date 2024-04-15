37bfa35c61245adba63b6c04a29602fca60404a0
AKSHATH P <akshathpkk@gmail.com>
2024-04-15 18:46:59 +0530

    added route for createchannel

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 import { toggleSubscription , getSubscribedChannels , getUserChannelSubscribers } from "../controllers/subscription.controller";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/toggleSubscription").post(toggleSubscription);
+router.route("/toggleSubscription").post(verifyJWT, toggleSubscription);
 router.route("/getSubscribedChannels").get(getSubscribedChannels)
-router.route("/getSubscribers").get(getUserChannelSubscribers)
+router.route("/getSubscribers").get(verifyJWT , getUserChannelSubscribers)
+router.route("/create").get(verifyJWT , getUserChannelSubscribers)
 
 export default router;
