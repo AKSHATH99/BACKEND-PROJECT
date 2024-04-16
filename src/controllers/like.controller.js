e61b74694e83f3a615685f5183016c0f3a9f6b37
AKSHATH P <akshathpkk@gmail.com>
2024-04-16 17:18:43 +0530

    fixed typo in import statement

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 import { isValidObjectId } from "mongoose";
 import { Like } from "../models/like.model.js";
-import { ApiError } from "../utils/apiError.js";
-import { ApiResponse } from "../utils/apiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
